a168b2340d90e31a03089c26225efc942caa3577
Евгений <e.hacko.nicecode@gmail.com>
2018-09-02 23:17:09 +0300

    URL was fixed

--- src/apps/articles/urls.py
+++ src/apps/articles/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from apps.articles.views import ArticleView
 
 urlpatterns = [
-    path('^article/<int:pk>/', ArticleView.as_view(), name='articles')
+    path('article/<int:pk>/', ArticleView.as_view(), name='articles')
 ]
